bbfa37f5fa574d6e5e5a608b2b3e36f7418b786c
andredelima7 <andre.nogueira.cruz@escola.pr.gov.br>
2025-09-12 12:28:20 -0300

    Add files via upload

--- andre-2d/conexao.php
+++ andre-2d/conexao.php
@@ -14,4 +14,12 @@ if (!$conexao) {
 
 // Ajustar charset para suportar acentos
 mysqli_set_charset($conexao, "utf8");
+
+$cloud_name = "deybunoxn";
+$api_key    = "998575748394411";
+$api_secret = "GlZ_EE9vUaQDM6WHw6-55nivknE
+
+
+"; 
+
 ?>
